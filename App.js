6b286312ae2a54f28ec7ca6dcf5e06ce9cd56359
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-18 13:49:47 -0300

    Criado o layout da calculadora

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Button from './src/components/Button';
+import Display from './src/components/Display';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+    state = {
+        displayValue: '0',
+    };
+
+    addDigit = (n) => {
+        this.setState({ displayValue: n });
+    };
+
+    clearMemory = () => {
+        this.setState({ displayValue: '0' });
+    };
+
+    setOperation = (operation) => {};
+
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Display value={this.state.displayValue} />
+                <View style={styles.buttons}>
+                    <Button label="AC" triple onClick={this.clearMemory} />
+                    <Button
+                        label="/"
+                        operation
+                        onClick={this.setOperation}
+                    />
+                    <Button label="7" onClick={this.addDigit} /> 
+                    <Button label="8" onClick={this.addDigit} />
+                    <Button label="9" onClick={this.addDigit} />
+                    <Button
+                        label="*"
+                        operation
+                        onClick={this.setOperation}
+                    />
+                    <Button label="4" onClick={this.addDigit} />
+                    <Button label="5" onClick={this.addDigit} />   
+                    <Button label="6" onClick={this.addDigit} />
+                    <Button
+                        label="-"
+                        operation
+                        onClick={this.setOperation}
+                    />
+                    <Button label="1" onClick={this.addDigit} />
+                    <Button label="2" onClick={this.addDigit} />
+                    <Button label="3" onClick={this.addDigit} />
+                    <Button
+                        label="+"
+                        operation
+                        onClick={this.setOperation}
+                    />
+                    <Button label="0" double onClick={this.addDigit} />
+                    <Button label="." onClick={this.addDigit} />
+                    <Button
+                        label="="
+                        operation
+                        onClick={this.setOperation}
+                    />
+                </View>
+                <StatusBar style="auto"></StatusBar>
+            </SafeAreaView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
 });
