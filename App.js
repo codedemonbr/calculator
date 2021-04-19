9688e5ee7606b58a60bc1ded0fb0db7a4a54ad26
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-19 07:25:15 -0300

    Calculadora finalizada

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import Button from './src/components/Button';
-import Display from './src/components/Display';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Button from "./src/components/Button";
+import Display from "./src/components/Display";
+
+const initialState = {
+    displayValue: "0",
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0,
+};
 
 export default class App extends Component {
     state = {
-        displayValue: '0',
+        ...initialState,
     };
 
     addDigit = (n) => {
-        this.setState({ displayValue: n });
+        if (n === "." && this.state.displayValue.includes(".")) {
+            return;
+        }
+        const clearDisplay =
+            this.state.displayValue === "0" || this.state.clearDisplay;
+
+        const currentValue = clearDisplay ? "" : this.state.displayValue;
+        const displayValue = currentValue + n;
+        this.setState({ displayValue, createDisplay: false });
+
+        if (n !== ".") {
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[this.state.current] = newValue;
+            this.setState({ values });
+        }
     };
 
     clearMemory = () => {
-        this.setState({ displayValue: '0' });
+        this.setState({ ...initialState });
     };
 
-    setOperation = (operation) => {};
+    setOperation = (operation) => {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true });
+        } else {
+            const equals = operation === "=";
+            const values = [...this.state.values];
+            try {
+                values[0] = eval(
+                    `${values[0]} ${this.state.operation} ${values[1]}`
+                );
+            } catch (e) {
+                values[0] = this.state.values[0];
+            }
+
+            values[1] = 0;
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals, //pode ser true para limpar o display
+                values,
+            });
+        }
+    };
 
     render() {
         return (
@@ -25,42 +71,22 @@ export default class App extends Component {
                 <Display value={this.state.displayValue} />
                 <View style={styles.buttons}>
                     <Button label="AC" triple onClick={this.clearMemory} />
-                    <Button
-                        label="/"
-                        operation
-                        onClick={this.setOperation}
-                    />
-                    <Button label="7" onClick={this.addDigit} /> 
+                    <Button label="/" operation onClick={this.setOperation} />
+                    <Button label="7" onClick={this.addDigit} />
                     <Button label="8" onClick={this.addDigit} />
                     <Button label="9" onClick={this.addDigit} />
-                    <Button
-                        label="*"
-                        operation
-                        onClick={this.setOperation}
-                    />
+                    <Button label="*" operation onClick={this.setOperation} />
                     <Button label="4" onClick={this.addDigit} />
-                    <Button label="5" onClick={this.addDigit} />   
+                    <Button label="5" onClick={this.addDigit} />
                     <Button label="6" onClick={this.addDigit} />
-                    <Button
-                        label="-"
-                        operation
-                        onClick={this.setOperation}
-                    />
+                    <Button label="-" operation onClick={this.setOperation} />
                     <Button label="1" onClick={this.addDigit} />
                     <Button label="2" onClick={this.addDigit} />
                     <Button label="3" onClick={this.addDigit} />
-                    <Button
-                        label="+"
-                        operation
-                        onClick={this.setOperation}
-                    />
+                    <Button label="+" operation onClick={this.setOperation} />
                     <Button label="0" double onClick={this.addDigit} />
                     <Button label="." onClick={this.addDigit} />
-                    <Button
-                        label="="
-                        operation
-                        onClick={this.setOperation}
-                    />
+                    <Button label="=" operation onClick={this.setOperation} />
                 </View>
                 <StatusBar style="auto"></StatusBar>
             </SafeAreaView>
@@ -73,7 +99,7 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     buttons: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
+        flexDirection: "row",
+        flexWrap: "wrap",
     },
 });
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, Dimensions, TouchableHighlight } from "react-native";
 
 const styles = StyleSheet.create({
     button: {
         fontSize: 40,
-        height: Dimensions.get('window').width / 4,
-        width: Dimensions.get('window').width / 4,
+        height: Dimensions.get("window").width / 4,
+        width: Dimensions.get("window").width / 4,
         padding: 20,
-        backgroundColor: '#f0f0f0',
-        textAlign: 'center',
+        backgroundColor: "#f0f0f0",
+        textAlign: "center",
         borderWidth: 1,
-        borderColor: '#888',
+        borderColor: "#888",
     },
     operationButton: {
-        color: '#fff',
-        backgroundColor: '#fa8231',
+        color: "#fff",
+        backgroundColor: "#fa8231",
     },
     buttonDouble: {
-        width: (Dimensions.get('window').width / 4) * 2,
+        width: (Dimensions.get("window").width / 4) * 2,
     },
     buttonTriple: {
-        width: (Dimensions.get('window').width / 4) * 3,
+        width: (Dimensions.get("window").width / 4) * 3,
     },
 });
 
